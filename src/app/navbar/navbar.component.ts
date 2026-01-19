c0088fe26df5390fd083c352738be6a31f01e9d6
Moxesh <moxesh.mehta@artemhealthtech.com>
2026-01-19 18:36:00 +0530

    First Push

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MenubarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/home'
+      },
+      {
+        label: 'About',
+        icon: 'pi pi-star',
+        routerLink: '/about'
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+        routerLink: '/contact'
+      }
+    ];
+  }
+}
