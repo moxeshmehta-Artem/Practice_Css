f92498925bda53ead8d40ad5806aa7ff476a7d05
Moxesh <moxesh.mehta@artemhealthtech.com>
2026-01-19 18:47:01 +0530

    card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, InputTextModule, InputTextareaModule, ButtonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
